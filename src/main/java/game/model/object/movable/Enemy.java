754dc0dce89435fa55285011a406c5855b0bca2a
Neelarghya Mandal <neelarghya.mandal@thoughtworks.com>
2019-02-20 16:11:47 +0530

    Neel | Added a minimum speed to Enemies

--- src/main/java/game/model/object/movable/Enemy.java
+++ src/main/java/game/model/object/movable/Enemy.java
@@ -11,12 +11,13 @@ import static game.model.object.ObjectType.ENEMY;
 public class Enemy extends MovableObject {
     private static final int WIDTH = 20;
     private static final int HEIGHT = 20;
+    private static final int MINIMUM_SPEED = 1;
 
     public Enemy(double x, double y) {
         super(x, y, ENEMY, new MovementConstants(3, 3, 6, 1));
         Random random = new Random();
-        xSpeed = random.nextDouble() * getMovementConstants().getMaxSpeed();
-        ySpeed = random.nextDouble() * getMovementConstants().getMaxSpeed();
+        xSpeed = random.nextDouble() * (getMovementConstants().getMaxSpeed() - MINIMUM_SPEED) + MINIMUM_SPEED;
+        ySpeed = random.nextDouble() * (getMovementConstants().getMaxSpeed() - MINIMUM_SPEED) + MINIMUM_SPEED;
         this.addBehaviour(new Rebound(this));
     }
 
