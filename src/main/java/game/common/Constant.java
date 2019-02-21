23ff6654dfd358bc1895e3a37cbda0674b893d67
Neelarghya Mandal <neelarghya.mandal@thoughtworks.com>
2019-02-22 00:43:41 +0530

    Neel | Named the game... :D

--- src/main/java/game/common/Constant.java
+++ src/main/java/game/common/Constant.java
@@ -1,7 +1,7 @@
 package game.common;
 
 public class Constant {
-    public static final String TITLE = "Place holder for Title";
+    public static final String TITLE = "Snake in a Bullet Hell";
     private static double ASPECT_RATIO = 12.0 / 9.0;
     public static int WINDOW_WIDTH = 960;
     public static int WINDOW_HEIGHT = (int) (WINDOW_WIDTH / ASPECT_RATIO);
--- src/main/java/game/common/CustomColors.java
+++ src/main/java/game/common/CustomColors.java
@@ -4,6 +4,6 @@ import java.awt.*;
 
 public interface CustomColors {
     Color translucentGrey = new Color(0.7f, 0.7f, 0.7f, .6f);
-    Color translucentWhite = new Color(1.0f, 1.0f, 1.0f, .8f);
+    Color translucentWhite = new Color(1.0f, 1.0f, 1.0f, .7f);
     Color translucentGreen = new Color(0.0f, 1f, 0.0f, .5f);
 }
